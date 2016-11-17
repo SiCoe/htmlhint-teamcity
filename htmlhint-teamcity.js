1392d3bb978f5cfa42217dad19c5fc164ea3e1b4
Simon Coe <simon.coe@avelo.com>
2016-11-17 16:43:46 +0000

    update output to also be grouped by rule broken

--- htmlhint-teamcity.js
+++ htmlhint-teamcity.js
@@ -3,7 +3,13 @@ var path = require('path');
 
 var REPORTER = 'HtmlHint';
 
-module.exports = function (file) {
+function addRule(rules, rule) {
+    rule.errors = [];
+    rules.push(rule);
+    return rule;
+}
+
+module.exports = file => {
     if (file.htmlhint && file.htmlhint.errorCount > 0) {
 
         tsm.testSuiteStarted({ name: REPORTER });
@@ -12,16 +18,32 @@ module.exports = function (file) {
 
         tsm.testSuiteStarted({ name: filePath });
 
-        file.htmlhint.messages.forEach(function (message) {
-            var error = message.error;
-            tsm.testStarted({ name: error.rule.description })
-                .testFailed({
-                    name: "line " + error.line + ", col " + error.col + ", " + error.message,
-                    message: error.message,
-                    detailed: "evidence:" + error.evidence + " (" + error.rule.link + ")"
-                })
-                .testFinished({ name: error.rule.description });
-        });
+        file.htmlhint.messages
+            .reduce((currentRules, message) => {
+                //create array of rules with their associated errors
+                (
+                    currentRules.find(rule => message.error.rule.id === rule.id) ||
+                    addRule(currentRules, message.error.rule)
+                ).errors.push(message.error);
+                return currentRules;
+            }, [])
+            .forEach(rule => {
+                //iterate over the array of rules and output new testFailed for each error on that rule
+                tsm.testSuiteStarted({ name: rule.description });
+
+                rule.errors.forEach(error => {
+                    var name = "line " + error.line + ", col " + error.col + ", " + error.message;
+                    tsm.testStarted({ name: name })
+                        .testFailed({
+                            name: name,
+                            message: error.message,
+                            detailed: "evidence:" + error.evidence + " (" + error.rule.link + ")"
+                        })
+                        .testFinished({ name: name });
+                });
+
+                tsm.testSuiteFinished({ name: rule.description });
+            });
 
         tsm.testSuiteFinished({ name: filePath });
         tsm.testSuiteFinished({ name: REPORTER });
